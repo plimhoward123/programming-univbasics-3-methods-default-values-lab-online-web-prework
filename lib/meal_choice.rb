ceaa933255e638b37fb0a4c3638b84dd4b1547f0
Plimon Howard V <plimon.howard@gmail.com>
2019-09-04 16:19:35 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,8 @@
 # Your code here
+def meal(veg1,veg2,protien)
+  puts "What a nutritious meal!"
+  if protien != ""
+    return "A plate of #{protein} with #{veg1} and #{veg2}."
+  else
+    return "A plate of with #{veg1} and #{veg2}"
+end
